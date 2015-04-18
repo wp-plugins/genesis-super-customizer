5fa7c665e7efe5a072cd4a2cc4b71127793f842a
Mario Giancini <Mario Giancini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-18 04:06:33 +0000

    Adding first version of Genesis Super Customizer
    git-svn-id: https://plugins.svn.wordpress.org/genesis-super-customizer/trunk@1137730 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,84 @@
+=== Genesis Super Customizer ===
+Contributors: mariogiancini
+Donate link: http://supercustomizer.com/
+Tags: customizer, genesis, options, supercustomizer, design
+Requires at least: 3.8
+Tested up to: 4.1
+Stable tag: 4.1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+The easiest way to customize the design of your Genesis theme.
+
+
+== Description ==
+
+Genesis Super Customizer adds options to customize colors, create a fixed header that shrinks on scroll, upload logos, and much more, all through the native Wordpress Customizer interface.
+It's the easiest way to customize the design of your Genesis Theme. Please note this plugin requires the Genesis theme to be activated.
+
+More information can be found at [http://supercustomizer.com](http://supercustomizer.com "Genesis Super Customizer").
+
+
+== Installation ==
+
+1. Upload genesis-super-customzer.zip to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Use the Wordpress Customizer to change and adjust settings.
+
+
+== Frequently Asked Questions ==
+
+= How do I install a customizer template? =
+
+From your WordPress admin navigate to the Genesis > Import/Export menu. Click "Choose File"
+and location your .json customizer template file. Click "Upload File and Import". You should see
+a success notification on the top.
+
+= How do I reset the genesis super customizer options? =
+
+In the current version, you need to upload the default settings .json file. You can find that template at
+Import the same as you would any other template (instructions above). There a future plans for a simple reset button.
+
+= Does it work with premium Genesis child themes? =
+
+At the moment Genesis Super Customizer isn't tested with premium child themes.
+It should work fine with your custom child theme, but not all options on premium themes work.
+
+
+== Screenshots ==
+
+
+== Support ==
+
+Please follow on Twitter [@SuperCustomizer]( http:/twitter.com/SuperCustomizer ) for the latest updates.
+
+Detailed documentation is coming soon.
+
+
+== Changelog ==
+
+= 1.0 - April 18, 2015 =
+* Initial release.
+
+
+== Upgrade Notice ==
+
+
+== Future Updates ==
+
+Here are some planned updates for the near future
+
+* Instant Preview
+* Filters For Developers To Add Their Own Settings
+* Additional Menu customizations for submenus
+* Additional Content customizations for entry meta, blockquotes, and more.
+* Genesis Premium Child Theme compatibility
+* Compatibility with other Featured Genesis plugins
+* Center aligned fixed header
+* Change Featured Image And Title Order
+* Shortcodes for adding theme colors throughout your site
+* Favicon Upload
+* Reset Button (instead of uploading default settings)
+* WooCommerce compatibility
+* More extensions for other popular plugins
+* Requests by you! Send requests on Twitter @SuperCustomizer.
---
+++ public/class-gsc-header-image.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+* Register header image customizer settings
+*
+* @link       http://genesissupercustomizer.com
+* @since      1.0.0
+*
+* @package    Genesis_Super_Customizer
+* @subpackage Genesis_Super_Customizer/includes
+*/
+
+class GSC_Header_Image extends GSC_Base {
+
+  protected $settings_field = 'genesis-customizer-settings';
+
+  protected $new_section = false;
+
+  protected $mod_section = 'header_image';
+
+  //* Placeholders for variable attributes
+  private $logo_width_max = 1200;
+
+  //* Setup mods here to get for output.
+  protected function get_mods() {
+
+    if( $this->get_field_value( 'wrap_width' ) ) {
+      $this->logo_width_max = $this->get_field_value( 'wrap_width' );
+    }
+
+    $this->mod_settings = array(
+      'logo_width' => array(
+        'css'         => array( '.title-area', 'width', '', 'px', true, array( 'media_query' => 'min-width: ' . $this->desktop_min_size . 'px' ) ),
+        'priority'    => 10,
+        'type'        => 'range',
+        'default'     => 360,
+        'input_attrs' => array(
+          'min'   => 100,
+          'max'   => $this->logo_width_max,
+          'step'  => 10,
+        ),
+        'description' => 'If you adjust this you may have to re-upload your logo for the new size. Max since depends on content wrap width.',
+        'option'      => true,
+      ),
+      'logo_height' => array(
+        'css'         => array( '.title-area', 'height', '', 'px', true, array(
+          // 'media_query'   => 'min-width: ' . $this->desktop_min_size . 'px',
+          'affects'       => array( '.site-header .genesis-nav-menu > li > a', '.header-image .title-area', '.header-image .site-title > a'  ),
+          'affects_values'=> array( '.site-header .genesis-nav-menu > li > a' => 'line-height: ' .  $this->get_field_value( 'logo_height' ) . 'px; padding-top: 0px; padding-bottom: 0px;', '.header-image .title-area' => 'padding: 0;', '.header-image .site-title > a' => 'min-height: initial; height: ' .  $this->get_field_value( 'logo_height' ) . 'px;' )
+        ) ),
+        'priority'    => 10,
+        'type'        => 'range',
+        'default'     => 80,
+        'input_attrs' => array(
+          'min'   => 40,
+          'max'   => 200,
+          'step'  => 5,
+        ),
+        'description' => 'If you adjust this you may have to re-upload your logo for the new size.',
+        'option'      => true,
+      ),
+      'flex_crop' => array(
+        'css'         => array(),
+        'priority'    => 10,
+        'type'        => 'checkbox',
+        'default'     => 0,
+        'label'       => 'Enable Flexable Image Cropping',
+        'option'      => true,
+      ),
+      'header_hover_opacity' => array(
+        'css'         => array( '.custom-header .site-title a:hover', 'opacity' ),
+        'priority'    => 10,
+        'type'        => 'range',
+        'default'     => 100,
+        'label'       => 'Header Logo Hover Opacity',
+        'description' => 'Have the Site Title / Logo fade when hovering over it. Choose Opacity 0% to 100%.',
+        'input_attrs' => array(
+          'min'   => 0,
+          'max'   => 100,
+          'step'  => 5,
+        ),
+        'decimal'     => true,
+        'option'      => true,
+      ),
+      'fullwidth_image' => array(
+        'css'         => array( '.title-area', 'width', '', '% !important', true, array(
+          'checkbox'        => true,
+          'value'           => '100',
+        ) ),
+        'priority'    => 10,
+        'type'        => 'checkbox',
+        'default'     => 0,
+        'label'       => 'Make Header Image Full Width',
+        'option'      => true,
+      ),
+      'center_image' => array(
+        'css'         => array( '.header-image .site-title > a', 'background-position', '', '% !important', true, array(
+          'checkbox'        => true,
+          'value'           => '50',
+        ) ),
+        'priority'    => 10,
+        'type'        => 'checkbox',
+        'default'     => 0,
+        'label'       => 'Center The Header Image',
+        'option'      => true,
+      )
+    );
+
+  }
+
+} // end class
+
+new GSC_Header_Image;
